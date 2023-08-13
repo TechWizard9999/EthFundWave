44c551553b8878ac5d96a19f345172dee26b84e9
Sridhar Suthapalli <sutapalli.sns@gmail.com>
2023-08-13 18:53:28 +0530

    "Added springboot pack"

---
+++ server/springboot/ethfundwave/src/main/java/com/ethfundwave/ethfundwave/EthfundwaveApplication.java
@@ -0,0 +1,13 @@
+package com.ethfundwave.ethfundwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthfundwaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthfundwaveApplication.class, args);
+	}
+
+}
